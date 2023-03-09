eeaa27d1177d309a01a68e65e72ee90a054411f3
AugustoPB <106416779+AugustoPBaldino@users.noreply.github.com>
2023-03-08 23:02:55 -0300

    Funcionalidade dos botões
    Botão de adicionar tarefa, concluir tarefa e remover tarefa funcionando.

---
+++ To_do_List/script.js
@@ -0,0 +1,66 @@
+//Seleção de elementos
+const toDoForm = document.querySelector("#to-do-form");
+const toDoInput = document.querySelector("#to-do-input");
+const toDoList = document.querySelector("#to-do-list");
+const editForm = document.querySelector("#edit-input");
+const cancelEditBtn = document.querySelector("#cancel-edit-btn");
+// Funções
+const saveTodo = (text) => {
+
+    const toDo = document.createElement("div")
+    toDo.classList.add("to-do");
+
+    const toDoTitle = document.createElement("h3");
+    toDoTitle.innerText = text;
+    toDo.appendChild(toDoTitle);
+
+    const doneBtn = document.createElement("button")
+    doneBtn.classList.add("finish-to-do")
+    doneBtn.innerHTML = '<i class="fa-solid fa-check"></i>'
+    toDo.appendChild(doneBtn);
+
+    const editBtn = document.createElement("button")
+    editBtn.classList.add("edit-to-do")
+    editBtn.innerHTML = '<i class="fa-solid fa-pen"></i>'
+    toDo.appendChild(editBtn);
+
+    const deleteBtn = document.createElement("button")
+    deleteBtn.classList.add("remove-to-do")
+    deleteBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>'
+    toDo.appendChild(deleteBtn)
+
+    toDoList.appendChild(toDo);
+
+    toDoInput.value = "";
+    toDoInput.focus();
+} 
+
+// Eventos
+toDoForm.addEventListener("submit",(e) => {
+
+    e.preventDefault()
+
+    const inputValue = toDoInput.value;
+
+    if(inputValue) {
+        saveTodo(inputValue);
+    }
+});
+
+document.addEventListener("click", (e) =>{
+
+    const targetEl = e.target
+    const parentEl = targetEl.closest("div");
+
+    if(targetEl.classList.contains("finish-to-do")){
+        parentEl.classList.toggle("done");
+    }
+
+    if(targetEl.classList.contains("remove-to-do")){
+        parentEl.remove();
+    }
+
+    if(targetEl.classList.contains("edit-to-do")){
+        //parentEl.
+    }
+});
